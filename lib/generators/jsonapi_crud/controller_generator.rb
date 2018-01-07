ecb7bf9eb1c4b40578fc3f122b87df8b1fb1acc8
Jacob Bullock <jacob.bullock@gmail.com>
2018-01-06 19:22:21 -0700

    removing erb_template for creating controller classes

--- lib/generators/jsonapi_crud/controller_generator.rb
+++ lib/generators/jsonapi_crud/controller_generator.rb
@@ -14,32 +14,14 @@ module JsonapiCrud
     def create_controller
       output = File.join(JsonapiCrud.configuration.controller_output, "#{controller_file_name}_controller.rb")
 
+      @config = {:base_class => JsonapiCrud.configuration.base_class,
+                 :controller_class_name => controller_class_name,
+                 :modules => JsonapiCrud.configuration.controller_modules,
+                 :params => options[:params]}
 
-
-      erb_template("controller.rb.erb",
-                   output,
-                   {:base_class => JsonapiCrud.configuration.base_class,
-                           :controller_class_name => controller_class_name,
-                           :modules => JsonapiCrud.configuration.controller_modules,
-                           :params => options[:params]})
+      template "controller.rb.erb", output
 
       puts "Controller created at #{output}"
-    end
-
-    private
-
-    def erb_template(source, destination, config)
-      if File.exist?(destination) && !options[:force]
-        raise Exception.new('A file already exists at that location, use force: true to overwrite it')
-      end
-      source_content = File.read( File.expand_path("../../templates/#{source}", __FILE__) )
-      template = ERB.new(source_content).result(binding)
-
-      if destination.index('/')
-        path = destination[0..(destination.rindex('/')-1)]
-        Dir.mkdir(path) unless Dir.exist?(path) || path == ".."
-      end
-      File.write(destination, template)
     end
 
   end
