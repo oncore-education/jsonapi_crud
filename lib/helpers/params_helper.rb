06b5baf5b70df03a28e60602cfc9b3c24e16e2d7
Jacob Bullock <jacob.bullock@gmail.com>
2018-01-15 19:39:56 -0700

    adding getters for relationships

--- lib/helpers/params_helper.rb
+++ lib/helpers/params_helper.rb
@@ -6,15 +6,20 @@ module JsonapiCrud
     end
 
     def p_attributes
-      p_data[:attributes]
+      p_data[:attributes] || ActionController::Parameters.new
     end
 
     def p_relationships
       p_data[:relationships]
     end
 
-    def p_meta
-      params[:meta]
+    def p_relatonship(r)
+      p_data[p_relationships[r]]
+    end
+
+    def p_meta(source = nil)
+      p_data(source)[:meta]
+      #params[:meta]
     end
 
     def p_include
