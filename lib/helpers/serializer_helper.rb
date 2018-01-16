d8f246d4e4daee9b12b1d1a85decf0401b29fe8e
Jacob Bullock <jacob.bullock@gmail.com>
2018-01-15 19:39:15 -0700

    helper methods to get serialized values

---
+++ lib/helpers/serializer_helper.rb
@@ -0,0 +1,36 @@
+module JsonapiCrud
+  module SerializerHelper
+
+    def self.included(base)
+      class << base
+
+        def serializer_type
+          "#{self.name.underscore}_serializer".classify.constantize
+        end
+
+        def serializer
+          obj =  self.name.constantize.new
+          self.serializer_type.new(obj)
+        end
+
+        def serialized_attributes
+          self.serializer.attributes.keys
+        end
+
+        def valid_attributes
+          private_attributes = []
+          if serializer_type.method_defined? :private_attributes
+            private_attributes = serializer.private_attributes.map { |attr| attr.split(":").first.to_sym }
+          end
+
+          self.serialized_attributes.reject { |key| key.to_s == "id" } + private_attributes
+        end
+
+        def serialized_relationships
+          self.serializer.associations.map{ |assoc| assoc.name }
+        end
+      end
+    end
+
+  end
+end
