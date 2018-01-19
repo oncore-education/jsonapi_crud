ffa4082a784e28f48751a4b28bc1fad93f670af0
Jacob Bullock <jacob.bullock@gmail.com>
2018-01-19 01:56:59 -0700

    refactoring methods for checking if one model is related to another

---
+++ lib/helpers/active_record_helper.rb
@@ -0,0 +1,30 @@
+module JsonapiCrud
+  module ActiveRecordHelper
+
+    def editable_relationships
+      self.serialized_relationships.select{ |name| name.to_s.pluralize == name.to_s }
+    end
+
+    def can_update_relationship?(key)
+      self.send(key).nil? || editable_relationships.include?(key.to_sym)
+    end
+
+    def valid_relationships
+      self.serialized_relationships
+    end
+
+    def can_relate?(key)
+      return false unless can_update_relationship?(key)
+      self.serialized_relationships.include? key.to_sym
+
+      # a = valid_relationships
+      # if a.is_a?(Hash)
+      #   a = a[params[:action].to_sym] || []
+      # end
+      # a.include?(key.to_sym)
+    end
+
+  end
+end
+
+
